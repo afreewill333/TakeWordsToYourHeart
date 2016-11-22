c40caef2a967304b6b2e9dc85112c4893f539331
John Green <afreewill.me@gmail.com>
2016-11-22 15:01:52 +0800

    Add files via upload

---
+++ TakeWordsToYourHeart.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class TakeWordsToYourHeart {
+
+	public static void main(String[] args){
+		Integer unitcount=42;
+		Integer teamcount=1;
+		Integer threshold=5;
+		List<Integer> ruler = new ArrayList<Integer>(Arrays.asList(1,2,3,4,7,12,21));
+		List days = new LinkedList<List>();
+
+		for(int i=0;i<unitcount;i++){
+			List temp = new LinkedList<Integer>();
+			
+			for(int j=0;j<teamcount;j++){
+				temp.add(i+1);
+				i++;
+			days.add(temp);
+			
+
+
+		System.out.println("It is a Test ~ .");
+	}
+}
+
+
